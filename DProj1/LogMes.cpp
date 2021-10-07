6e88444d5db92b223ee6a6c4a9423296ced0ecde
MinnDi <61431354+MinnDi@users.noreply.github.com>
2021-10-07 23:22:24 +0300

    Add files via upload
    Logger logic

---
+++ DProj1/LogMes.cpp
@@ -0,0 +1,41 @@
+/*!
+ \version 1.3
+ \date 02.10.2021
+ \ref LoggerController
+ \brief Метод с логикой логирования
+ \param message Сохраняемое сообщение
+ \throw SaveException При ошибке сохранения
+ \throw NullPointerException При передаче пустого сообщения
+ \code
+ log("Hi!");
+ log(message);
+ \endcode
+ */
+    public void log(Message message) throws SaveException, NullPointerException {
+        try {
+            if (message == null) {
+                throw new NullPointerException("There is no message");
+            } else if (message.getValue() == null) {
+                throw new NullPointerException("Message is empty");
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        try {
+            if (state == State.NULL) {
+                lastMessage = message;
+                state = State.NOT_NULL;
+            } else if (message.typeEquals(lastMessage)) {
+                lastMessage.accumulate(message);
+            } else {
+                lastMessage.close();
+                saver.save(lastMessage.decorate());
+                lastMessage = message;
+            }
+        }
+        catch (SaveException e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
